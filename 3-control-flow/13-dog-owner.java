f566b5f014a166075a97e299af878f8c953db469
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 16:55:18 +0800

    Create 13-dog-owner.java

---
+++ 3-control-flow/13-dog-owner.java
@@ -0,0 +1,17 @@
+// Syzu 4/11/25
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        boolean isCatOwner = false;
+        boolean isDogOwner = true;
+
+        if(isCatOwner) {
+            System.out.println("Use the code MEOW for 20% off cat items");
+        } else if (isDogOwner) {
+            System.out.println("Use the code WOOF fro 20% off dog items");
+        } else {
+            System.out.println("Welcome to the pet store!");
+        } 
+    }
+}
