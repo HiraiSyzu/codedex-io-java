5cdd258aef63335db69f86de6202369058a4cb3f
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 15:18:10 +0800

    Update 4-error-logs.java

--- 1-Hello-World/4-error-logs.java
+++ 1-Hello-World/4-error-logs.java
@@ -1,1 +1,14 @@
+// Error Logs 😑
+// Codédex
+// Syzu 4/11/25
 
+public class HelloWorld {
+    public static void main(String[] args) {
+       System.err.println("Connecting to server…"); // The program attempts to establish a connection to a server.
+       System.err.println("Error: Connection timed out."); // The attempt to connect to the server failed because the connection took too long.
+       System.err.println("Retrying…"); // The program is going to restart.
+
+        // System.err.println() is used to print error messages! 😵😵❌
+        // It is similar to System.out.println() but is used to print error messages.
+    }
+}
