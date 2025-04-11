89fc1f90c83cefbb54aa6d41236c8bdbe97a4b88
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 15:40:15 +0800

    Create 7-id-card.java

---
+++ 2-java-variables/7-id-card.java
@@ -0,0 +1,22 @@
+// ID-Card
+// Syzu 4/11/25
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        
+        String name = "Syzu";
+        String state = "Metro Manila";
+        char gender = 'M';
+        int age = 17;
+        boolean organDonor = false;
+
+        // Output
+
+        System.out.println(name);
+        System.out.println(state);
+        System.out.println(gender);
+        System.out.println(age);
+        System.out.println(organDonor);
+    }
+}
