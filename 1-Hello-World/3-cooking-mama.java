185f82f61006ff1378c12c86bea3fa98c0c5e4d9
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 15:16:29 +0800

    Create 3-cooking-mama.java

---
+++ 1-Hello-World/3-cooking-mama.java
@@ -0,0 +1,14 @@
+// Cooking Mama 👩‍🍳
+// Codédex
+//Syzu 4/11/25
+
+public class HelloWorld {
+    public static void main(String[] args) {
+       System.out.println("1. Boil water.");
+        System.out.println("2. Add pasta.");
+        System.out.println("3. Cook for 112 minutes.");
+        System.out.println("4. Remove water from pasta.");
+        System.out.println("5. Add sauce.");
+        System.out.println("6. Mix it.");
+    }
+}
