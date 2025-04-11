19f37e592960d2362d6b74f5d1fc0456486acaa4
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 17:52:58 +0800

    Create 14-rollercoaster.java

---
+++ 3-control-flow/14-rollercoaster.java
@@ -0,0 +1,15 @@
+//Syzu 4/11/25
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        int height = 140;
+        int weight = 40;
+
+        if (height >= 120 && weight >= 40){
+            System.out.println("Congrats! You can ride!");
+        } else {
+            System.out.println("Sorry, You can't ride today.");
+        }
+    }
+}
