0637819bbcce0b65f305799ae9d4872e5597d8d1
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 15:26:11 +0800

    Create 5-music-wrapped.java

---
+++ 1-Hello-World/5-music-wrapped.java
@@ -0,0 +1,25 @@
+// Music Wrapped 🎵
+// Codédex
+// Syzu 4/11/25
+
+/**
+ * Woah? What's this? 🤔 It's a weird looking comment. Another *, eh?
+ * Write a little bit about yourself here. What's your name? Where are you from? What do you do?
+ *  
+ * I'm a comment, and I want to say hi! 👋
+ */
+
+public class HelloWorld {
+  public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+
+        System.out.println("What is Love"); // TWICE
+        System.out.println("I'm Missing You"); // Sunjae
+        System.out.println("Every Summertime"); // NIKI
+        System.out.println("Hot"); // LE SSERAFIM
+        System.out.println("Fall in Love Alone" // Stacey Ryan
+
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+  
+  }
+}
