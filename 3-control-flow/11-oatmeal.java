61ee99a0e3b79b6f193af8bb8514f3f9cf4ce074
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 16:25:57 +0800

    Create 11-oatmeal.java

---
+++ 3-control-flow/11-oatmeal.java
@@ -0,0 +1,14 @@
+//Syzu 4/11/25
+
+public class HelloWorld{
+  public static void main(String[] args){
+
+    // Paste code below 💖
+      int oats = 2;
+
+    if (oats > 0) {
+      System.out.println("Let's make oatmeal today!");
+      
+    }
+  }
+}
