e98cf761d757dd060ce02c1043bff734a25c9f0d
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 19:15:06 +0800

    Create 16-rainbow-nums.java

---
+++ 4-loops/16-rainbow-nums.java
@@ -0,0 +1,13 @@
+// Syzu 4/11/25
+
+// Paste code below! 💖
+public class HelloWorld {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 5) { // Since function of while loop is executing the code until it become false
+          System.out.println(num); // Output: 5
+          num = num + 1;  
+        }
+    }
+}
