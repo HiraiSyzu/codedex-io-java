67bdfd1de89672530406eb6a74d83a11cf828991
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 15:35:44 +0800

    Create 6-storage-box.java

---
+++ 2-java-variables/6-storage-box.java
@@ -0,0 +1,19 @@
+// Syzu 4/11/25
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        
+        // Variable int value
+
+        int livingRoom = 15;
+        int masterBedroom = 45;
+        int bathroom = 10;
+
+        // Output
+
+        System.out.println(livingRoom);
+        System.out.println(masterBedroom);
+        System.out.println(bathroom);
+    }
+}
+
