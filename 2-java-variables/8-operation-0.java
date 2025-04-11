eace9b0b89962a28ba9925f8d2087ca7e9e1c9d0
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 15:49:17 +0800

    Create 8-operation-0.java

---
+++ 2-java-variables/8-operation-0.java
@@ -0,0 +1,17 @@
+//Syzu 4/11/25
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+      
+        //  Variable Double Value:
+        double chemicalA = 50.45;
+        double chemicalB = 10.12;
+
+        // Formula (A + B) / (A * B)
+        double reactionResult = (chemicalA + chemicalB) / (chemicalA * chemicalB);
+
+        // Output
+        System.out.println(reactionResult);
+    }
+}
