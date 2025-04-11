18ceed37fcbf00746b3076882ba73d45c5d8b53b
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-11 19:42:50 +0800

    Create 17-all-the-while.java

---
+++ 4-loops/17-all-the-while.java
@@ -0,0 +1,14 @@
+// Syzu 4/11/25
+
+public class HelloWorld {
+  public static void main(String[] args) {
+
+    // Var Int Value
+    int i = 1;
+
+    while (i <= 15) {
+      System.out.println("Line " + i+ ": LET'S GO TWICE IMNIDA");
+      i = i + 1;
+    }
+  }
+}
