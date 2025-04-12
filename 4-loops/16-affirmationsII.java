14b2738283ce400fe61df07e117b2e2fd056a883
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-12 11:39:33 +0800

    Create 16-affirmationsII.java

---
+++ 4-loops/16-affirmationsII.java
@@ -0,0 +1,12 @@
+// Syzu 4/12/25
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+
+        for (int i = 0; i <= 50; i++) {
+            System.out.println("I will be great at programming!");       
+        }
+        
+    }
+}
