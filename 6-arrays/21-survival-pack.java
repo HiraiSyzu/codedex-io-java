1e8ecfc8992ae2cba60b4f03f231aa4950835eba
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-18 13:27:46 +0800

    Update 21-survival-pack.java

--- 6-arrays/21-survival-pack.java
+++ 6-arrays/21-survival-pack.java
@@ -1,3 +1,23 @@
 //Syzu 4/18/25
 
+// Syzu 4/18/25
+
+// 21. Survival Pack
+
+// Another Note: Array index doesnt start with 1, it always start with 0, So Thats why its only survivalPack[0] - survivalPack[4] not 1-5
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+    // String Array
+    String [] survivalPack = {"Water Bottle", "First Aid Kit", "Flashlight", "Energy Bars", "Knife"};
+
+    //Println Method
+    System.out.println(survivalPack[0]); // Print: "Water Bottle"
+    System.out.println(survivalPack[1]); // Print: "First Aid Kit"
+    System.out.println(survivalPack[2]); // Print: "Flashlight"
+    System.out.println(survivalPack[3]); // Print: "Energy Bars"
+    System.out.println(survivalPack[4]); // Print: "Knife"
+  }
+}
 
